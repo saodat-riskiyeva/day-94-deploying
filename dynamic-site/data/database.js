ac941806c8f1153b924435fbf0c1542195628b22
SR <saodat.riskiyeva@gmail.com>
2025-10-15 18:27:24 -0400

    using environment variables for mongodb

--- dynamic-site/data/database.js
+++ dynamic-site/data/database.js
@@ -1,17 +1,23 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
+
+let mongodbUrl = "mongodb://localhost:27017";
+
+if (process.env.MONGODB_URL) {
+  mongodbUrl = process.env.MONGODB_URL;
+}
 
 let database;
 
 async function initDatabase() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('deployment');
+  const client = await MongoClient.connect(mongodbUrl);
+  database = client.db("deployment");
 }
 
 function getDb() {
   if (!database) {
-    throw new Error('No database connected!');
+    throw new Error("No database connected!");
   }
 
   return database;
